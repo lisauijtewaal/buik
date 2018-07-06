2baff9f43febe27dd495d39439c1b7dcc8e78ef4
Lisa Uijtewaal <lisa_uijtewaal@hotmail.com>
2018-07-06 13:12:04 +0200

    fixing bugs

--- src/pages/loginPage/index.jsx
+++ src/pages/loginPage/index.jsx
@@ -5,6 +5,12 @@ import './index.css';
 
 
 class App extends Component {
+
+  scrollToTop(){
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   render() {
     return (
       <div className="App">
